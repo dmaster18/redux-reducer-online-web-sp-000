c8831e87ee8a73a6c57f3f2cf094237325a3bf62
David Strauch <dmaster18@gmail.com>
2021-01-16 20:53:42 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,5 @@
 export function managePresents(action, state = {numberOfPresents: 0}){
-  switch(action.type) {
-    case 'INCREASE': 
-      return {...state, state.numberOfPresents += 1}
-    case default: 
-      return state
+  if (action.type === 'INCREASE') {
+    return {...state, numberOfPresents: (state.numberOfPresents + 1)};
   }
 }
